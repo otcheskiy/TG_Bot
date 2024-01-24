21e760532b660df8e6605ba6c0524cb6728f616f
Sergey O <sa@osergey>
2024-01-25 01:37:53 +0300

    add kb, filters

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ load_dotenv(find_dotenv())
 
 #пользовательские модули
 from handlers.user_private import user_private_router
+from handlers.user_group import user_group_router
 from common.bot_cmds_list import private
 
 #print( 'token_tg: ', token_tg)
@@ -24,6 +25,7 @@ bot = Bot(token = os.getenv('TOKEN'))
 dp = Dispatcher()
 
 dp.include_router(user_private_router)
+dp.include_router(user_group_router)
 
 
 async def main():
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,51 +1,77 @@
 from aiogram import F, types, Router
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import CommandStart, Command, or_f
+from filters.chat_types import ChatTypeFilter
+
+from kbds import reply
 
 user_private_router = Router()
+user_private_router.message.filter(ChatTypeFilter(['private']))
 
 @user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message):
-    #print(CommandStart.commands.__dir__)
-    await message.answer( text = f"Привет, {message.from_user.full_name}, я виртуальный помощник" )
+    await message.answer( text = f"Привет, {message.from_user.full_name}, я виртуальный помощник",
+                         reply_markup=reply.test_kb )
+    """await message.answer( text = f"Привет, {message.from_user.full_name}, я виртуальный помощник",
+                         reply_markup=reply.start_kb3.as_markup(
+                             resize_keyboard=True,
+                             input_field_placeholder='Что Вас интересует ?') )"""
 
 @user_private_router.message(Command('help'))
 async def handle_help(message: types.Message):
     text = 'Я эхо бот.\nОтправь мне сообщение!'
     await message.answer(text = text)
 
-@user_private_router.message(Command('menu'))
+
+# @user_private_router.message(F.text.lower() == 'меню')
+@user_private_router.message( or_f (Command('menu'), (F.text.lower() == 'меню' ) ))
 async def handle_menu(message: types.Message):
     text = 'Вот меню:'
-    await message.answer(text = text)
+    await message.answer(text = text, reply_markup=reply.del_kbd)
 
+
+@user_private_router.message(F.text.lower() == 'о магазине')
 @user_private_router.message(Command('about'))
 async def handle_about(message: types.Message):
-    text = 'О нас:'
+    text = 'О магазине:'
     await message.answer(text = text)
 
+@user_private_router.message(F.text.lower() == 'варианты оплаты')
 @user_private_router.message(Command('payments'))
 async def handle_payment(message: types.Message):
     text = 'Варианты оплаты:'
     await message.answer(text = text)
 
+@user_private_router.message(
+        (F.text.lower().contains('доставк')) | (F.text.lower() == 'варианты доставки')
+)
 @user_private_router.message(Command('shipping'))
 async def handle_payment(message: types.Message):
     text = 'Варианты доставки:'
     await message.answer(text = text)
 
-"""@user_private_router.message()
-async def echo(message: types.Message):
-    text = message.text
-    #print(text)
-    #text = f"Привет, {message.from_user.full_name}"
-    if text in [ 'Привет', 'привет', 'hi', 'hello', 'Hello']:
-        await message.answer( text = f"И тебе привет!, {message.from_user.full_name}" )
-    elif text in ['Пока', 'пока', 'До свидания']:
-        await message.answer( text = f"До новых встреч, {message.from_user.last_name}")
-    else:
-        await message.answer(text)"""
+@user_private_router.message(F.contact)
+async def get_contact(message: types.Message):
+    await message.answer(f"номер получен")
+    await message.answer(str(message.contact))
+
+
+@user_private_router.message(F.location)
+async def get_location(message: types.Message):
+    await message.answer(f"локация получена")
+    await message.answer(str(message.location))
 
 @user_private_router.message(F.photo)
 async def menu_cmd(message: types.Message):
     text = 'Это магический фильтр'
     await message.answer(text = text)
+
+@user_private_router.message(F.text)
+async def echo(message: types.Message):
+    text_low = message.text.lower()
+    text = message.text
+    if text_low in [ 'привет', 'здорово', 'здравствуйте', 'hi', 'hello']:
+        await message.answer( text = f"И тебе привет!, {message.from_user.full_name}" )
+    elif text_low in ['пока', 'до свидания']:
+        await message.answer( text = f"До новых встреч, {message.from_user.last_name}")
+    else:
+        await message.answer(text)
